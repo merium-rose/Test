529f95ac5d1de6dbe83242f45fb1642734259222
merium-rose <meriumrose@yahoo.com>
2021-07-07 11:10:55 -0500

    new

--- Suzybackup.py
+++ Suzybackup.py
@@ -8,8 +8,14 @@ print(cal)
 for x in cal:
     print("edf-anonymize.exe " + "'" + x + "'" + " 'DeID/" + x + "'" + " 'Mae Jemison' " + x[0:8])
     #print (len(x)
-    subprocess.run("edf-anonymize.exe " + "'" + x + "'" + " 'DeID/" + x + "'" + " 'Mae Jemison' " + x[0:8], shell=True, check=True)
-    subprocess.run("pwd", shell=True, check=True)
+    #subprocess.run("edf-anonymize.exe " + "'" + x + "'" + " 'DeID/" + x + "'" + " 'Mae Jemison' " + x[0:8], shell=True, check=True)
+    #subprocess.run("pwd", shell=True, check=True)
+    
+for File in os.listdir("."):
+    if File.endswith(".pages"):
+        print("edf-anonymize.exe " + "'" + x + "'" + " 'DeID/" + x + "'" + " 'Mae Jemison' " + x[0:8])         
+    else:
+        print("false")
 
 print ("DONE")
 #subprocess.run("edf-anonymize.exe " + "'" + x + "'" + " 'DeID/" + x + "'" + " 'Mae Jemison' " + x[0:8])
