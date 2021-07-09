0006dfbe0a4ba63bf9b0c77d6908a1533a9c2101
merium-rose <meriumrose@yahoo.com>
2021-07-09 17:02:04 -0500

    final push

--- Suzybackup.py
+++ Suzybackup.py
@@ -1,20 +1,27 @@
 #This code requires the edf-anonymize.exe installed in Path and computer
-#Code also requires the os and subprocess modules
+#Code also requires the os, sys, and subprocess modules
 #!/Library/Frameworks/Python.framework/Versions/3.9/bin/python3
 
 import os
 import subprocess
+import sys
 
 cal = os.listdir('.')
 
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+PatientID = sys.argv[1]
+print 'Argument 3 is' , PatientID
+
 print(cal)
 
-for x in cal:
-    print("edf-anonymize.exe " + " '" + x + "'" + " 'DeID/" + x + "'" + " 'Mae Jemison' " + x[0:8])
-    subprocess.run("edf-anonymize.exe " + " '" + x + "'" + " 'DeID/" + x + "'" + " 'Mae Jemison' " + x[0:8], shell=True)
+#for x in cal:
+    #print("edf-anonymize.exe " + " '" + x + "'" + " 'DeID/" + x + "'" + " 'Mae Jemison' " + x[0:8])
+    #subprocess.run("edf-anonymize.exe " + " '" + x + "'" + " 'DeID/" + x + "'" + " 'Mae Jemison' " + x[0:8], shell=True)
 
 for File in os.listdir("."):
-    if File.endswith(".pages"):
-        print("edf-anonymize.exe " + " '" + File + "'" + " 'DeID/" + File + "'" + " 'Mae Jemison' " + File[0:8])
+    if File.endswith(".edf"):
+        print("edf-anonymize.exe " + " '" + File + "'" + " 'DeID/" + File + "'" + ' ' + "'" + PatientID + "'" + ' ' + File[0:8])
+        
 
 print ("DONE")
